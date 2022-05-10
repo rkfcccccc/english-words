651036ca152112790c9cdfdb917fb91812638dcc
Egor Konyk <e.konyk1@gmail.com>
2022-05-10 17:35:15 +0500

    user service: created postgresql repository implementation

---
+++ backend/services/user/internal/user/postgres_repository.go
@@ -0,0 +1,70 @@
+package user
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/georgysavva/scany/pgxscan"
+	"github.com/jackc/pgx/v4/pgxpool"
+)
+
+const usersTable = "users"
+
+type PostgresRepository struct {
+	db *pgxpool.Pool
+}
+
+func NewPostgresRepository(db *pgxpool.Pool) Repository {
+	return &PostgresRepository{db}
+}
+
+func (repo *PostgresRepository) Create(ctx context.Context, email, password string) (int, error) {
+	var userId int
+
+	query := fmt.Sprintf("INSERT INTO %s (email, password, register_date) VALUES ($1, $2, $3) RETURNING id", usersTable)
+	err := pgxscan.Get(ctx, repo.db, &userId, query, email, password, time.Now().Unix())
+
+	if err != nil {
+		return -1, fmt.Errorf("pgxscan.Get: %v", err)
+	}
+
+	return userId, nil
+}
+
+func (repo *PostgresRepository) GetById(ctx context.Context, userId int) (*User, error) {
+	var user User
+
+	query := fmt.Sprintf("SELECT * FROM %s WHERE id=$1", usersTable)
+	err := pgxscan.Get(ctx, repo.db, &user, query, userId)
+
+	if err != nil {
+		return nil, fmt.Errorf("pgxscan.Get: %v", err)
+	}
+
+	return &user, nil
+}
+
+func (repo *PostgresRepository) GetByEmail(ctx context.Context, email string) (*User, error) {
+	var user User
+
+	query := fmt.Sprintf("SELECT * FROM %s WHERE email=$1", usersTable)
+	err := pgxscan.Get(ctx, repo.db, &user, query, email)
+
+	if err != nil {
+		return nil, fmt.Errorf("pgxscan.Get: %v", err)
+	}
+
+	return &user, nil
+}
+
+func (repo *PostgresRepository) Delete(ctx context.Context, userId int) error {
+	query := fmt.Sprintf("DELETE FROM %s WHERE id=$1", usersTable)
+	_, err := repo.db.Exec(ctx, query, userId)
+
+	if err != nil {
+		return fmt.Errorf("repo.db.Exec: %v", err)
+	}
+
+	return nil
+}
