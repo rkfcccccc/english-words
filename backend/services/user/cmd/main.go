0fcf50759ab5bfa4f6f127300c4e4400f4a55ad8
Egor Konyk <e.konyk1@gmail.com>
2022-05-10 20:03:27 +0500

    user service: main initialization

--- backend/services/user/cmd/main.go
+++ backend/services/user/cmd/main.go
@@ -7,34 +7,35 @@ import (
 	"os"
 
 	"github.com/joho/godotenv"
-	pb "github.com/rkfcccccc/english_words/proto/users"
+	"github.com/rkfcccccc/english_words/user/internal/user"
 	"github.com/rkfcccccc/english_words/user/pkg/postgres"
 	"google.golang.org/grpc"
 )
 
-type Server struct {
-	pb.UnimplementedUserServiceServer
-}
-
 func main() {
-	if err := godotenv.Load(); err != nil {
+	if err := godotenv.Load("../../.env"); err != nil {
 		log.Fatalf("failed to load .env: %v", err)
 	}
 
 	db := postgres.NewPool(
-		context.Background(), os.Getenv("POSTGRES_USERNAME"), os.Getenv("POSTGRES_PASSWORD"),
-		os.Getenv("POSTGRES_HOST"), os.Getenv("POSTGRES_PORT"), os.Getenv("USER_SERVICE_DB"),
+		context.Background(), os.Getenv("POSTGRES_USER"), os.Getenv("POSTGRES_PASSWORD"),
+		os.Getenv("POSTGRES_HOST"), os.Getenv("POSTGRES_PORT"), os.Getenv("POSTGRES_DB"),
 	)
 
-	listener, err := net.Listen("tcp", ":50050")
+	repo := user.NewPostgresRepository(db)
+	service := user.NewService(repo)
+	server := user.NewServer(service)
+
+	listener, err := net.Listen("tcp", os.Getenv("USER_GRPC_ADDR"))
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	server := grpc.NewServer()
+	s := grpc.NewServer()
+	server.Register(s)
 
 	log.Printf("listening at %v", listener.Addr())
-	if err := server.Serve(listener); err != nil {
+	if err := s.Serve(listener); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
 }
