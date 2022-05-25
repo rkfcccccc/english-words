48d4c06d4265ccec8af724607da9fe946b879d46
Egor Konyk <e.konyk1@gmail.com>
2022-05-25 13:06:40 +0500

    dictionary service: added entry model

---
+++ backend/services/dictionary/internal/dictionary/entry.go
@@ -0,0 +1,20 @@
+package dictionary
+
+type Definition struct {
+	Text    string `json:"text"`
+	Example string `json:"example"`
+}
+
+type Meaning struct {
+	PartOfSpeech string       `json:"partOfSpeech"`
+	Definitions  []Definition `json:"definitions"`
+	Synonyms     []string     `json:"synonyms"`
+	Antonyms     []string     `json:"antonyms"`
+}
+
+type WordEntry struct {
+	Id       string    `bson:"_id,omitempty"`
+	Word     string    `json:"word"`
+	Phonetic string    `json:"phonetic"`
+	Meanings []Meaning `json:"meanings"`
+}
