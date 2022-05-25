5e7ca91cc436a34756d903de8840c8c67a9fae73
Egor Konyk <e.konyk1@gmail.com>
2022-05-26 00:05:35 +0500

    dictionary service: created main initialization

---
+++ backend/services/dictionary/cmd/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/rkfcccccc/english_words/services/dictionary/internal/dictionary"
+	"github.com/rkfcccccc/english_words/services/dictionary/pkg/dictionaryapi"
+	"github.com/rkfcccccc/english_words/shared_pkg/dsync/redsync"
+	"github.com/rkfcccccc/english_words/shared_pkg/mongodb"
+	"github.com/rkfcccccc/english_words/shared_pkg/redis"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	if err := godotenv.Load("../../.env"); err != nil {
+		log.Fatalf("failed to load .env: %v", err)
+	}
+
+	db, err := mongodb.NewClient(
+		context.Background(), os.Getenv("MONGO_USER"), os.Getenv("MONGO_PASSWORD"),
+		os.Getenv("MONGO_HOST"), os.Getenv("MONGO_PORT"), os.Getenv("MONGO_DB"),
+	)
+
+	if err != nil {
+		log.Fatalf("failed to connect mongodb: %v", err)
+	}
+
+	redis := redis.NewClient(os.Getenv("REDIS_HOST"), os.Getenv("REDIS_PORT"))
+	sync := redsync.NewClient(redis)
+	dict := dictionaryapi.NewClient()
+
+	repo := dictionary.NewMongoRepository(db.Collection("dictionary"))
+	service := dictionary.NewService(repo, sync, dict)
+	server := dictionary.NewServer(service)
+
+	listener, err := net.Listen("tcp", os.Getenv("DICTIONARY_GRPC_ADDR"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	s := grpc.NewServer()
+	server.Register(s)
+
+	log.Printf("listening at %v", listener.Addr())
+	if err := s.Serve(listener); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
