b6dd9c03d2dbb4a5857d8eba96da4dc0522a2357
Egor Konyk <e.konyk1@gmail.com>
2022-07-30 01:36:46 +0500

    added error handling for user grpc calls

--- backend/services/gateway/internal/service/user/client.go
+++ backend/services/gateway/internal/service/user/client.go
@@ -2,17 +2,28 @@ package user
 
 import (
 	"context"
+	"errors"
 	"log"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
 
 	pb "github.com/rkfcccccc/english_words/proto/user"
+	"github.com/rkfcccccc/english_words/services/gateway/internal/service/base"
 )
+
+var ErrAlreadyExists = errors.New("email already in use")
+var ErrInvalidEmail = errors.New("invalid email")
+var ErrTooLongPassword = errors.New("too long password")
+var ErrNotFound = errors.New("user was not found")
 
 type Client struct {
 	conn   *grpc.ClientConn
 	client pb.UserServiceClient
+	base.Client
 }
 
 func NewClient(addr string) *Client {
@@ -22,21 +33,42 @@ func NewClient(addr string) *Client {
 	}
 
 	client := pb.NewUserServiceClient(conn)
-	return &Client{conn, client}
+	return &Client{conn: conn, client: client}
 }
 
 func (c *Client) Create(ctx context.Context, email, password string) (int, error) {
-	response, err := c.client.Create(ctx, &pb.CreateRequest{Email: email, Password: password})
+	var trailer metadata.MD
+	response, err := c.client.Create(ctx, &pb.CreateRequest{Email: email, Password: password}, grpc.Trailer(&trailer))
+
+	switch c.GetErrorName(trailer) {
+	case "ALREADY_EXISTS":
+		return -1, ErrAlreadyExists
+	case "INVALID_EMAIL":
+		return -1, ErrInvalidEmail
+	case "TOO_LONG_PASSWORD":
+		return -1, ErrTooLongPassword
+	}
+
 	return int(response.GetUserId()), err
 }
 
 func (c *Client) GetById(ctx context.Context, userId int) (*User, error) {
 	response, err := c.client.GetById(ctx, &pb.GetByIdRequest{UserId: int32(userId)})
+
+	if status.Code(err) == codes.NotFound {
+		return nil, ErrNotFound
+	}
+
 	return transformFromGRPC(response), err
 }
 
 func (c *Client) GetByEmail(ctx context.Context, email string) (*User, error) {
 	response, err := c.client.GetByEmail(ctx, &pb.GetByEmailRequest{Email: email})
+
+	if status.Code(err) == codes.NotFound {
+		return nil, ErrNotFound
+	}
+
 	return transformFromGRPC(response), err
 }
 
