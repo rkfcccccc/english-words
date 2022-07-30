6b85dcb2812522ac521f2a4436f695c1b3818b32
Egor Konyk <e.konyk1@gmail.com>
2022-07-30 18:41:48 +0500

    Initial frontend commit

---
+++ frontend/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Sizer(
+      builder: (context, orientation, deviceType) => MaterialApp(),
+    );
+  }
+}
