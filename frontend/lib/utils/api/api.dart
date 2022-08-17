32605e409f6d801e49c0c8d3778e782d06c761c3
Egor Konyk <e.konyk1@gmail.com>
2022-08-17 13:06:55 +0500

    Add authRequired parameter to api request

--- frontend/lib/utils/api/api.dart
+++ frontend/lib/utils/api/api.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:english_words/utils/api/user/user.dart';
 import 'package:english_words/utils/errors.dart';
 
 const authority = "localhost:8080";
@@ -27,46 +28,66 @@ dynamic request(
   String path, {
   Map<String, dynamic>? params,
   Map<String, dynamic>? body,
+  authRequired = false,
 }) async {
   final uri = Uri.http(authority, basePath + path, params);
 
-  try {
-    final request = await client.openUrl(httpMethod, uri);
+  while (true) {
+    try {
+      final request = await client.openUrl(httpMethod, uri);
 
-    request.headers.add("accept", ContentType.json);
+      while (refreshLock) {
+        await Future.delayed(const Duration(milliseconds: 100));
+        continue;
+      }
 
-    if (body != null) {
-      request.headers.contentType = ContentType.json;
-      request.write(jsonEncode(body));
-    }
+      request.headers.add("accept", ContentType.json);
 
-    final response = await request.close();
-    if (response.statusCode >= 500) {
+      if (authRequired) {
+        final accessToken = await getAccessToken();
+        request.headers.add("Authorization", "Bearer ${accessToken!}");
+      }
+
+      if (body != null) {
+        request.headers.contentType = ContentType.json;
+        request.write(jsonEncode(body));
+      }
+
+      final response = await request.close();
+      if (response.statusCode >= 500) {
+        throw const AppError(
+          key: FailureError.serverInternalError,
+          message: "Server internal error",
+        );
+      }
+
+      dynamic data;
+      final contentType = response.headers.contentType;
+      if (contentType != null && contentType.mimeType == "application/json") {
+        data = jsonDecode(await readResponse(response));
+      } else if (contentType != null) {
+        throw Exception(
+          "got unexpected content type: ${response.headers.contentType}",
+        );
+      }
+
+      if (response.statusCode == HttpStatus.unauthorized &&
+          data?["error_name"] == "TOKEN_EXPIRED") {
+        await refresh();
+        await Future.delayed(const Duration(milliseconds: 250));
+        continue;
+      }
+
+      if (response.statusCode >= 400) {
+        throwSomeAppError(data?["error_name"]);
+      }
+
+      return data;
+    } on SocketException {
       throw const AppError(
-        key: FailureError.serverInternalError,
-        message: "Server internal error",
+        key: FailureError.noInternet,
+        message: "No Internet Connection",
       );
     }
-
-    dynamic data;
-    final contentType = response.headers.contentType;
-    if (contentType != null && contentType.mimeType == "application/json") {
-      data = jsonDecode(await readResponse(response));
-    } else if (contentType != null) {
-      throw Exception(
-        "got unexpected content type: ${response.headers.contentType}",
-      );
-    }
-
-    if (response.statusCode >= 400) {
-      throwSomeAppError(data?["error_name"]);
-    }
-
-    return data;
-  } on SocketException {
-    throw const AppError(
-      key: FailureError.noInternet,
-      message: "No Internet Connection",
-    );
   }
 }
