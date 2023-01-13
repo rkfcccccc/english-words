e704be02d1f3d99e2480dc6c460e37d16b6245aa
e-konyk <46644159+rkfcccccc@users.noreply.github.com>
2023-01-13 17:29:25 +0500

    feat: populate with translations in kafka

--- backend/services/dictionary/cmd/kafka/main.go
+++ backend/services/dictionary/cmd/kafka/main.go
@@ -9,6 +9,7 @@ import (
 	"github.com/rkfcccccc/english_words/services/dictionary/pkg/pictureapi"
 	"github.com/rkfcccccc/english_words/services/dictionary/pkg/pictureapi/freepik"
 	"github.com/rkfcccccc/english_words/services/dictionary/pkg/pictureapi/unsplash"
+	"github.com/rkfcccccc/english_words/services/dictionary/pkg/reversoapi"
 	"github.com/rkfcccccc/english_words/shared_pkg/mongodb"
 	"github.com/segmentio/kafka-go"
 )
@@ -23,6 +24,7 @@ func main() {
 		log.Fatalf("failed to connect mongodb: %v", err)
 	}
 
+	reverso := reversoapi.NewClient()
 	pictures := pictureapi.NewService(freepik.NewPictureRepository(), unsplash.NewPictureRepository())
 
 	repo := dictionary.NewMongoRepository(db.Collection("dictionary"))
@@ -33,7 +35,7 @@ func main() {
 		log.Fatalf("failed to dial leader: %v", err)
 	}
 
-	consumer := dictionary.NewKafkaConsumer(service, pictures, os.Getenv("KAFKA_ADDR"))
+	consumer := dictionary.NewKafkaConsumer(service, pictures, reverso, os.Getenv("KAFKA_ADDR"))
 	if err := consumer.Serve(conn); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
--- backend/services/dictionary/internal/dictionary/kafka.go
+++ backend/services/dictionary/internal/dictionary/kafka.go
@@ -5,23 +5,28 @@ import (
 	"fmt"
 	"log"
 	"math/rand"
+	"sync"
 	"time"
 
 	"github.com/rkfcccccc/english_words/services/dictionary/pkg/pictureapi"
+	"github.com/rkfcccccc/english_words/services/dictionary/pkg/reversoapi"
 	models "github.com/rkfcccccc/english_words/shared_pkg/services/dictionary/models"
 	"github.com/segmentio/kafka-go"
+	"golang.org/x/sync/semaphore"
 )
 
+const MAX_CONCURRENT = 10
 const MAX_PICTURES = 10
 
 type Consumer struct {
 	service   *Service
 	pictures  *pictureapi.Service
+	reverso   reversoapi.Client
 	kafkaAddr string
 }
 
-func NewKafkaConsumer(service *Service, pictures *pictureapi.Service, kafkaAddr string) *Consumer {
-	return &Consumer{service, pictures, kafkaAddr}
+func NewKafkaConsumer(service *Service, pictures *pictureapi.Service, reverso reversoapi.Client, kafkaAddr string) *Consumer {
+	return &Consumer{service, pictures, reverso, kafkaAddr}
 }
 
 func NewKafkaProducer(kafkaAddr string) *kafka.Writer {
@@ -29,6 +34,10 @@ func NewKafkaProducer(kafkaAddr string) *kafka.Writer {
 }
 
 func (c *Consumer) populateWithPictures(ctx context.Context, entry *models.WordEntry) {
+	if entry.Pictures != nil {
+		return
+	}
+
 	pictures := c.pictures.Search(ctx, entry.Word)
 	rand.Shuffle(len(pictures), func(i, j int) {
 		pictures[i], pictures[j] = pictures[j], pictures[i]
@@ -48,10 +57,63 @@ func (c *Consumer) populateWithPictures(ctx context.Context, entry *models.WordE
 	}
 
 	if err := c.service.SetPictures(ctx, entry.Id, sourced); err != nil {
-		log.Printf("got error when processing word: %v", err)
+		log.Printf("got error when service.SetPictures: %v", err)
 	}
 
 	log.Printf("populated %s with %d pictures\n", entry.Word, count)
+}
+
+func (c *Consumer) populateWithTranslations(ctx context.Context, entry *models.WordEntry) {
+	if entry.Pictures != nil {
+		return
+	}
+
+	result, err := c.reverso.GetTranslation(ctx, entry.Word, "eng", "rus")
+	if err != nil {
+		log.Printf("got error when reverso.GetTranslation of %s: %v", entry.Word, err)
+		return
+	}
+
+	translations := make([]string, len(result.ContextResults.Results))
+	for i, result := range result.ContextResults.Results {
+		translations[i] = result.Translation
+	}
+
+	if err := c.service.SetTranslations(ctx, entry.Id, translations); err != nil {
+		log.Printf("got error when service.SetTranslations: %v", err)
+	}
+
+	log.Printf("populated %s with %d translations\n", entry.Word, len(translations))
+}
+
+func (c *Consumer) populateAll(ctx context.Context, entry *models.WordEntry) {
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+
+	go func() {
+		c.populateWithPictures(ctx, entry)
+		wg.Done()
+	}()
+
+	go func() {
+		c.populateWithTranslations(ctx, entry)
+		wg.Done()
+	}()
+
+	wg.Wait()
+}
+
+func (c *Consumer) processWord(wordId string) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	entry, err := c.service.GetById(ctx, wordId)
+	if err != nil {
+		log.Printf("service.GetById: %v", err)
+		cancel()
+		return
+	}
+
+	c.populateAll(ctx, entry)
+	cancel()
 }
 
 func (c *Consumer) Serve(conn *kafka.Conn) error {
@@ -62,24 +124,22 @@ func (c *Consumer) Serve(conn *kafka.Conn) error {
 		MaxBytes: 10e6, // 10MB
 	})
 
+	sem := semaphore.NewWeighted(20)
+
 	for {
-		m, err := r.ReadMessage(context.Background())
+		message, err := r.ReadMessage(context.Background())
 		if err != nil {
 			log.Printf("failed to read message: %v", err)
 			break
 		}
 
-		ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
-
-		entry, err := c.service.GetById(ctx, string(m.Value))
-		if err != nil {
-			log.Printf("service.GetById: %v", err)
-			cancel()
-			continue
+		if err := sem.Acquire(context.Background(), 1); err != nil {
+			log.Printf("failed to acquire semaphore: %v", err)
+			break
 		}
 
-		c.populateWithPictures(ctx, entry)
-		cancel()
+		c.processWord(string(message.Value))
+		sem.Release(1)
 	}
 
 	if err := conn.Close(); err != nil {
